f2a73fdfcc262738aa8f7ac59fecfe1b39064e50
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2022-12-29 15:19:50 +0500

    Fix fix_nm_ap_ssid

--- configs/usr/lib/wb-configs/fix_nm_ap_ssid.sh
+++ configs/usr/lib/wb-configs/fix_nm_ap_ssid.sh
@@ -4,13 +4,13 @@
 wb_ap_connection="/etc/NetworkManager/system-connections/wb-ap.nmconnection"
 short_sn=`wb-gen-serial -s`
 if [ -f $wb_ap_connection ]; then
-    local tmp_connection="/tmp/wb-ap.nmconnection"
+    tmp_connection="/tmp/wb-ap.nmconnection"
     cp -f $wb_ap_connection $tmp_connection
     # remove default SSID value as it is device specific ad affects md5 hash
     sed -i "/Wiren[Bb]oard-${short_sn}/d" $tmp_connection
     # remove timestamp
     sed -i "/timestamp/d" $tmp_connection
-    local MD5=$(md5sum "$tmp_connection" | cut -f 1 -d ' ')
+    MD5=$(md5sum "$tmp_connection" | cut -f 1 -d ' ')
     # remove old wb-ap without subnet address
     if [ "$MD5" == "2ed8e74107f2dc68fabe96cd0fe6b61c" ]; then
         rm -f $wb_ap_connection || true
@@ -23,8 +23,8 @@ fi
 # make new nmconnection if missing
 deleted_wb_ap="/etc/NetworkManager/system-connections/d12c8d3c-1abe-4832-9b71-4ed6e3c20885.nmmeta"
 if [ ! -f "$wb_ap_connection" ] && [ ! -f "$deleted_wb_ap" ]; then
-    local wb_ap_connection_template="/usr/lib/NetworkManager/system-connections/wb-ap.nmconnection"
-    local ssid="WirenBoard-${short_sn}"
+    wb_ap_connection_template="/usr/lib/NetworkManager/system-connections/wb-ap.nmconnection"
+    ssid="WirenBoard-${short_sn}"
     cp -f $wb_ap_connection_template $wb_ap_connection
     sed -i "s/^ssid=@SSID@$/ssid=${ssid}/" "$wb_ap_connection"
 fi
