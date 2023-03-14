6f768806b5d6a8369f0dd19f71313ad39f3cef8c
lostpoint <lostpoint@icloud.com>
2023-03-14 21:38:23 +0600

    add rootfs archive download feature

---
+++ configs/usr/lib/cgi-bin/download_rootfs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+MOUNT_DIR=$(mktemp -d -t "WB-RFS.XXXXXXX")
+
+function cleanup().
+{
+    sudo umount $MOUNT_DIR
+    rm -r $MOUNT_DIR
+}
+
+trap cleanup EXIT
+
+sudo mount /dev/mmcblk0p3 $MOUNT_DIR
+
+if [ -d "$MOUNT_DIR/etc" ]; then
+    echo "Status: 200"
+    echo 'Content-Disposition: attachment; filename="rootfs.tar.gz"'
+    echo "Content-Type: application/gzip"
+    echo ""
+    sudo tar -czf - $MOUNT_DIR
+else
+    echo "Status: 500"
+    echo ""
+    echo "Error mounting rootfs"
+fi
