259a3597a78641a7fda34950dbc1e008b7e6f24e
lostpoint <lostpoint@icloud.com>
2023-03-15 10:53:20 +0600

    add some linting to script

--- configs/usr/lib/cgi-bin/download_rootfs.sh
+++ configs/usr/lib/cgi-bin/download_rootfs.sh
@@ -6,20 +6,20 @@ MOUNT_DIR=$(mktemp -d -t "WB-RFS.XXXXXXX")
 
 function cleanup()
 {
-    sudo umount -f $MOUNT_DIR || true
-    rm -r $MOUNT_DIR
+    sudo umount -f "$MOUNT_DIR" || true
+    rm -r "$MOUNT_DIR"
 }
 
 trap cleanup EXIT
 
-sudo mount /dev/mmcblk0p3 $MOUNT_DIR
+sudo mount "/dev/mmcblk0p3" "$MOUNT_DIR"
 
 if [ -d "$MOUNT_DIR/etc" ]; then
     echo "Status: 200"
     echo 'Content-Disposition: attachment; filename="rootfs.tar.gz"'
     echo "Content-Type: application/gzip"
     echo ""
-    sudo tar -czf - $MOUNT_DIR
+    sudo tar -czf - "$MOUNT_DIR"
 else
     echo "Status: 500"
     echo ""
