a14e61b7c00324565025906830a33969e486f010
lostpoint <lostpoint@icloud.com>
2023-11-20 15:20:00 +0600

    add chmod 0744 for factoryreset.fit refactor factory_reset.py and fwupdate_upload.py

--- configs/usr/lib/cgi-bin/factory_reset.py
+++ configs/usr/lib/cgi-bin/factory_reset.py
@@ -4,17 +4,14 @@ import os
 import sys
 from cgi import FieldStorage
 
-if os.path.islink("/var/run/wb-watch-update.dir"):
-    RW_DIR = os.path.realpath("/var/run/wb-watch-update.dir")
-else:
-    RW_DIR = os.environ.get("UPLOADS_DIR", "/var/www/uploads")  # nginx user should has rw access
-
-TMP_DIR = os.path.join(RW_DIR, "state", "tmp")  # excluded from wb-watch-update
-os.makedirs(TMP_DIR, exist_ok=True)
-
 
 def _error(msg=""):
-    sys.stdout.write("Status: 400 %s\r\n\r\n" % msg)
+    sys.stdout.write("Status: 400 Bad Request\r\n\r\nBad Request: %s" % msg)
+    sys.exit(1)
+
+
+def _die(msg=""):
+    sys.stdout.write("Status: 500 Internal Server Error\r\n\r\nInternal Server Error: %s" % msg)
     sys.exit(1)
 
 
@@ -26,23 +23,38 @@ def to_chunks(fp, chunk_size=8192):
         yield bs
 
 
-form = FieldStorage(encoding="utf-8")
-do_factory_reset = "factory_reset" in form.keys() and str(form.getvalue("factory_reset")) == 'true'
-if not do_factory_reset:
-    _error("Incorrect request")
+def main():
+    if os.path.islink("/var/run/wb-watch-update.dir"):
+        RW_DIR = os.path.realpath("/var/run/wb-watch-update.dir")
+    else:
+        RW_DIR = os.environ.get("UPLOADS_DIR", "/var/www/uploads")  # nginx user should has rw access
 
-# we need to update-with-reboot in order to factory reset, so we're changing output directory to .wb_update
-RW_DIR = "/mnt/data/.wb-update/"
-os.makedirs(RW_DIR, exist_ok=True)
-# create flags file
-flags_file = os.path.join(RW_DIR, 'install_update.web.flags')
-with open(flags_file, "w") as flags_file_h:
-    flags_file_h.write('--factoryreset ')
+    TMP_DIR = os.path.join(RW_DIR, "state", "tmp")  # excluded from wb-watch-update
+    os.makedirs(TMP_DIR, exist_ok=True)
 
-# copy factoryreset.fit to RW_DIR
-fname = "webupd.fit"
-with open("/mnt/data/.wb-restore/factoryreset.fit", "rb") as fp_upload:
-    with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
-        for chunk in to_chunks(fp_upload):
-            fp_save.write(chunk)
-sys.stdout.write("Status: 200\r\n\r\n")
+    form = FieldStorage(encoding="utf-8")
+    do_factory_reset = "factory_reset" in form.keys() and str(form.getvalue("factory_reset")) == 'true'
+    if not do_factory_reset:
+        _error("no factory_reset=true in POST arguments")
+
+    # we need to update-with-reboot in order to factory reset, so we're changing output directory to .wb_update
+    RW_DIR = "/mnt/data/.wb-update/"
+    os.makedirs(RW_DIR, exist_ok=True)
+    # create flags file
+    flags_file = os.path.join(RW_DIR, 'install_update.web.flags')
+    with open(flags_file, "w") as flags_file_h:
+        flags_file_h.write('--factoryreset ')
+
+    # copy factoryreset.fit to RW_DIR
+    fname = "webupd.fit"
+    with open("/mnt/data/.wb-restore/factoryreset.fit", "rb") as fp_upload:
+        with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
+            for chunk in to_chunks(fp_upload):
+                fp_save.write(chunk)
+    sys.stdout.write("Status: 200\r\n\r\n")
+
+
+try:
+    main()
+except Exception as e:
+    _die(str(e))
--- configs/usr/lib/cgi-bin/fwupdate_upload.py
+++ configs/usr/lib/cgi-bin/fwupdate_upload.py
@@ -5,18 +5,16 @@ import sys
 import tempfile
 from cgi import FieldStorage
 
-if os.path.islink("/var/run/wb-watch-update.dir"):
-    RW_DIR = os.path.realpath("/var/run/wb-watch-update.dir")
-else:
-    RW_DIR = os.environ.get("UPLOADS_DIR", "/var/www/uploads")  # nginx user should has rw access
-
-TMP_DIR = os.path.join(RW_DIR, "state", "tmp")  # excluded from wb-watch-update
-os.makedirs(TMP_DIR, exist_ok=True)
-
 
 def _error(msg=""):
-    sys.stdout.write("Status: 400 %s\r\n\r\n" % msg)
+    sys.stdout.write("Status: 400 Bad Request\r\n\r\nBad Request: %s" % msg)
     sys.exit(1)
+
+
+def _die(msg=""):
+    sys.stdout.write("Status: 500 Internal Server Error\r\n\r\nInternal Server Error: %s" % msg)
+    sys.exit(1)
+
 
 
 def to_chunks(fp, chunk_size=8192):
@@ -41,34 +39,50 @@ class DiskFieldStorage(FieldStorage):
             return tempfile.TemporaryFile("w+", encoding=self.encoding, newline="\n", dir=location)
 
 
-form = DiskFieldStorage(encoding="utf-8")
-if "file" not in form.keys():  # get("file") does not work (due to FieldStorage internals)
-    _error("Incorrect request")
+def main():
+    if os.path.islink("/var/run/wb-watch-update.dir"):
+        RW_DIR = os.path.realpath("/var/run/wb-watch-update.dir")
+    else:
+        RW_DIR = os.environ.get("UPLOADS_DIR", "/var/www/uploads")  # nginx user should has rw access
 
-# handle extra POST arguments
-do_expand_rootfs = "expand_rootfs" in form.keys() and str(form.getvalue("expand_rootfs")) == 'true'
-do_factory_reset = "factory_reset" in form.keys() and str(form.getvalue("factory_reset")) == 'true'
-if do_factory_reset or do_expand_rootfs:
-    # we need to update-with-reboot in order to expand rootfs, so we're changing output directory to .wb_update
-    RW_DIR = "/mnt/data/.wb-update/"
-    os.makedirs(RW_DIR, exist_ok=True)
-    # create flags file
-    flags_file = os.path.join(RW_DIR, 'install_update.web.flags')
-    with open(flags_file, "w") as flags_file_h:
-        if do_factory_reset:
-            flags_file_h.write('--factoryreset ')
-        if do_expand_rootfs:
-            flags_file_h.write('--force-repartition ')
+    TMP_DIR = os.path.join(RW_DIR, "state", "tmp")  # excluded from wb-watch-update
+    os.makedirs(TMP_DIR, exist_ok=True)
 
-# handle upload
-uploading_file = form["file"]
-if hasattr(uploading_file, "filename") and hasattr(uploading_file, "file"):
-    fname = uploading_file.filename or "fwupdate"
-    fp_upload = uploading_file.file
-else:
-    _error("Incorrect request body")
 
-with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
-    for chunk in to_chunks(fp_upload):
-        fp_save.write(chunk)
-sys.stdout.write("Status: 200\r\n\r\n")
+    form = DiskFieldStorage(encoding="utf-8")
+    if "file" not in form.keys():  # get("file") does not work (due to FieldStorage internals)
+        _error("Incorrect request")
+
+    # handle extra POST arguments
+    do_expand_rootfs = "expand_rootfs" in form.keys() and str(form.getvalue("expand_rootfs")) == 'true'
+    do_factory_reset = "factory_reset" in form.keys() and str(form.getvalue("factory_reset")) == 'true'
+    if do_factory_reset or do_expand_rootfs:
+        # we need to update-with-reboot in order to expand rootfs, so we're changing output directory to .wb_update
+        RW_DIR = "/mnt/data/.wb-update/"
+        os.makedirs(RW_DIR, exist_ok=True)
+        # create flags file
+        flags_file = os.path.join(RW_DIR, 'install_update.web.flags')
+        with open(flags_file, "w") as flags_file_h:
+            if do_factory_reset:
+                flags_file_h.write('--factoryreset ')
+            if do_expand_rootfs:
+                flags_file_h.write('--force-repartition ')
+
+    # handle upload
+    uploading_file = form["file"]
+    if hasattr(uploading_file, "filename") and hasattr(uploading_file, "file"):
+        fname = uploading_file.filename or "fwupdate"
+        fp_upload = uploading_file.file
+    else:
+        _error("Incorrect request body")
+
+    with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
+        for chunk in to_chunks(fp_upload):
+            fp_save.write(chunk)
+    sys.stdout.write("Status: 200\r\n\r\n")
+
+
+try:
+    main()
+except Exception as e:
+    _die(str(e))
