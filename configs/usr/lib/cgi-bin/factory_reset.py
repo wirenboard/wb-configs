3d82e650c0fd1c5fad4689f55d0b1d8353166ccb
lostpoint <lostpoint@icloud.com>
2023-11-22 13:03:25 +0600

    try more clear approach to factory reset fit creation (hardlink+open+close instead of copy)

--- configs/usr/lib/cgi-bin/factory_reset.py
+++ configs/usr/lib/cgi-bin/factory_reset.py
@@ -15,42 +15,48 @@ def _die(msg=""):
     sys.exit(1)
 
 
-def to_chunks(fp, chunk_size=8192):
-    while True:
-        bs = fp.read(chunk_size)
-        if not bs:
-            break
-        yield bs
+def check_for_form(form):
+    try:
+        form.keys()
+    except Exception:
+        _error("No form in request")
 
 
-def main():
-    if os.path.islink("/var/run/wb-watch-update.dir"):
-        RW_DIR = os.path.realpath("/var/run/wb-watch-update.dir")
-    else:
-        RW_DIR = os.environ.get("UPLOADS_DIR", "/var/www/uploads")  # nginx user should has rw access
-
-    TMP_DIR = os.path.join(RW_DIR, "state", "tmp")  # excluded from wb-watch-update
-    os.makedirs(TMP_DIR, exist_ok=True)
-
-    form = FieldStorage(encoding="utf-8")
+def check_for_confirmation(form):
     do_factory_reset = "factory_reset" in form.keys() and str(form.getvalue("factory_reset")) == 'true'
     if not do_factory_reset:
         _error("no factory_reset=true in POST arguments")
 
+
+def main():
+    form = FieldStorage(encoding="utf-8")
+    check_for_form(form)
+    check_for_confirmation(form)
+
     # we need to update-with-reboot in order to factory reset, so we're changing output directory to .wb_update
     RW_DIR = "/mnt/data/.wb-update/"
     os.makedirs(RW_DIR, exist_ok=True)
+
     # create flags file
-    flags_file = os.path.join(RW_DIR, 'install_update.web.flags')
-    with open(flags_file, "w") as flags_file_h:
+    FLAGS_FILE = os.path.join(RW_DIR, 'install_update.web.flags')
+    with open(FLAGS_FILE, "w") as flags_file_h:
         flags_file_h.write('--factoryreset ')
 
+    # make hardlink to factoryreset.fit
+    SRC_FIT = "/mnt/data/.wb-restore/factoryreset.fit"
+    DST_FIT = os.path.join(RW_DIR, "webupd.fit")
+    os.link(SRC_FIT, DST_FIT)
+
+    # open/close it for writing to trigger wb-watch-update
+    with open(DST_FIT, "ab") as fp:
+        pass
+
     # copy factoryreset.fit to RW_DIR
-    fname = "webupd.fit"
-    with open("/mnt/data/.wb-restore/factoryreset.fit", "rb") as fp_upload:
-        with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
-            for chunk in to_chunks(fp_upload):
-                fp_save.write(chunk)
+    # fname = "webupd.fit"
+    # with open("/mnt/data/.wb-restore/factoryreset.fit", "rb") as fp_upload:
+    #     with open(os.path.join(RW_DIR, fname), "wb") as fp_save:  # wb-watch-update triggers on fd close
+    #         for chunk in to_chunks(fp_upload):
+    #             fp_save.write(chunk)
     sys.stdout.write("Status: 200\r\n\r\n")
 
 
