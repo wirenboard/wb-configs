26fbc996e8eb795cbf22156ff865fc0dd3958fb5
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-12-20 14:25:33 +0500

    Refactoring

--- configs/usr/lib/wb-configs/fix_nm_ap_ssid.sh
+++ configs/usr/lib/wb-configs/fix_nm_ap_ssid.sh
@@ -2,8 +2,33 @@
 
 wb_ap_ssid_prefix="${WB_AP_SSID_PREFIX:-WirenBoard}"
 
-# find RTL8723BU
-old_wifi=$(lsusb -d'0bda:b720')
+# find Wi-Fi module
+wifi_module="RTL8723BU"
+if [ -z $(lsusb -d'0bda:b720') ]; then
+    wifi_module="RTL8733BU"
+fi
+
+# Args:
+# - MD5 hash of wb-ap.nmconnection without ssid and timestamp
+# - Wi-Fi module name
+should_delete_config() {
+    # remove old wb-ap without subnet address
+    if [ "$1" == "2ed8e74107f2dc68fabe96cd0fe6b61c" ]; then
+        return 0
+    fi
+
+    # remove old wb-ap without channel
+    if [ "$1" == "f2a720dc98d3764efab777f060ca79c4" ]; then
+        return 0
+    fi
+
+    # remove old wb-ap with band and channels
+    if [ "$2" == "RTL8733BU" ] && [ "$1" == "1737c3e9aa4a887f59235c6b4867fa5d" ]; then
+        return 0
+    fi
+
+    return 1
+}
 
 # remove old wi-fi ap connection
 wb_ap_connection="/etc/NetworkManager/system-connections/wb-ap.nmconnection"
@@ -17,20 +42,9 @@ if [ -f $wb_ap_connection ]; then
     sed -i "/timestamp/d" $tmp_connection
     MD5=$(md5sum "$tmp_connection" | cut -f 1 -d ' ')
 
-    # remove old wb-ap without subnet address
-    if [ "$MD5" == "2ed8e74107f2dc68fabe96cd0fe6b61c" ]; then
+    should_delete_config $MD5 $wifi_module && {
         rm -f $wb_ap_connection || true
-    fi
-
-    # remove old wb-ap without channel
-    if [ "$MD5" == "f2a720dc98d3764efab777f060ca79c4" ]; then
-        rm -f $wb_ap_connection || true
-    fi
-
-    # remove old wb-ap with band and channels
-    if [ -z "$old_wifi"] && [ "$MD5" == "1737c3e9aa4a887f59235c6b4867fa5d" ]; then
-        rm -f $wb_ap_connection || true
-    fi
+    }
 
     # Bad generated ssid
     if [ -f $wb_ap_connection ]; then
@@ -48,7 +62,7 @@ if [ ! -f "$wb_ap_connection" ] && [ ! -f "$deleted_wb_ap" ]; then
     sed -i "s/^ssid=@SSID@$/ssid=${ssid}/" "$wb_ap_connection"
 
     # remove band and channel for new chipset
-    if [ -z "$old_wifi"]; then
+    if [ "$wifi_module" == "RTL8733BU" ]; then
         sed -i "/channel=1/d" "$wb_ap_connection"
         sed -i "/band=bg/d" "$wb_ap_connection"
     fi
