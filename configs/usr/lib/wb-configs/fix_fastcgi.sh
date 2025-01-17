021883433d21c11ae896d5e6bebfe6de554dd6cf
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2025-01-17 10:49:18 +0500

    Remove auth db related code

--- configs/usr/lib/wb-configs/fix_fastcgi.sh
+++ configs/usr/lib/wb-configs/fix_fastcgi.sh
@@ -2,7 +2,3 @@
 
 # grant access fastcgi sripts to mosquitto's unix socket
 /usr/sbin/usermod -aG mosquitto www-data
-
-# grant access fastcgi sripts to /var/wb-webui
-chgrp www-data /var/wb-webui
-chmod g+wx /var/wb-webui
