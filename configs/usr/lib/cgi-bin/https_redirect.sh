990529951b673ce1592977ef17c1d38c6cbbbe3e
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2025-01-24 10:30:50 +0500

    Remove HTTPS redirect script

--- configs/usr/lib/cgi-bin/https_redirect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sn=$(sudo wb-gen-serial -s | tr '[:upper:]' '[:lower:]')
-prefix=$(echo "$HTTP_HOST_IP" | sed 's/\./-/g')
-
-echo "Status: 200"
-echo "Content-type: text/plain"
-echo ""
-echo "$prefix.$sn.ip.xcvb.win"
