db2b0eb70a821c80c917141387382ae268954e34
Nikolay Korotkiy <sikmir@disroot.org>
2025-05-23 01:26:37 +0400

    Fix mosquitto configs from wb-configs-early.service

--- configs/usr/lib/wb-configs/fix_mosquitto.sh
+++ configs/usr/lib/wb-configs/fix_mosquitto.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+MOSQUITTO_CONF="/etc/mosquitto/mosquitto.conf"
+LISTENERS_CONF='/etc/mosquitto/conf.d/10listeners.conf'
+restart_required=0
+if grep -q "log_dest file /var/log/mosquitto/mosquitto.log" $MOSQUITTO_CONF; then
+    sed -i 's#log_dest file /var/log/mosquitto/mosquitto.log#log_dest syslog#' $MOSQUITTO_CONF
+    restart_required=1
+fi
+if grep -q "pid_file /var/run/mosquitto.pid" $MOSQUITTO_CONF; then
+    sed -i 's#pid_file /var/run/mosquitto.pid#pid_file /run/mosquitto/mosquitto.pid#' $MOSQUITTO_CONF
+    restart_required=1
+fi
+if ! grep -q -F "include_dir /usr/share/wb-configs/mosquitto" $MOSQUITTO_CONF; then
+    sed -i '\#include_dir /etc/mosquitto/conf.d#iinclude_dir /usr/share/wb-configs/mosquitto' $MOSQUITTO_CONF
+    restart_required=1
+fi
+if grep -q "persistence .*" $MOSQUITTO_CONF; then
+    sed -i 's@persistence .*@# persistence is disabled by default. enable in /etc/mosquitto/conf.d/000persistence.conf@' $MOSQUITTO_CONF
+    restart_required=1
+fi
+if grep -q "^listener 18883$" $LISTENERS_CONF; then
+	sed -i '/^listener 18883$/clistener 18883 lo' $LISTENERS_CONF
+	restart_required=1
+fi
+
 mosquitto_confs=("/etc/mosquitto/acl.conf"
                  "/etc/mosquitto/passwd.conf"
                  "/etc/mosquitto/conf.d/auth.conf"
@@ -29,3 +53,18 @@ for i in ${!mosquitto_confs[@]}; do
         fi
     fi
 done
+
+# Fix permissions for default ACL and password files
+MOSQUITTO_ACL_CONF="/etc/mosquitto/acl/default.conf"
+MOSQUITTO_PASSWD_CONF="/etc/mosquitto/passwd/default.conf"
+
+chmod 0700 $MOSQUITTO_ACL_CONF
+chmod 0700 $MOSQUITTO_PASSWD_CONF
+
+chgrp mosquitto $MOSQUITTO_ACL_CONF
+chgrp mosquitto $MOSQUITTO_PASSWD_CONF
+
+chown mosquitto $MOSQUITTO_ACL_CONF
+chown mosquitto $MOSQUITTO_PASSWD_CONF
+
+exit "$restart_required"
